40808c9e771b0c5aa311633d10e58fb47b4ac3cc
geraldofori <oforige@gmail.com>
2025-09-09 15:54:26 +0000

    chore: implemented the sections to render in the home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
+import HeroSection from "@/components/ui/sections/HeroSection";
+import LifeProcessSection from "@/components/ui/sections/LifeProcessSection";
+import SermonSection from "@/components/ui/sections/SermonSection";
+import UpcomingSection from "@/components/ui/sections/UpcomingSection";
+import WeeklySection from "@/components/ui/sections/WeeklySection";
+import WelcomeSection from "@/components/ui/sections/WelcomeSection";
 
 export default function Home() {
   return (
-    <div>New Project</div>
+    <>
+    <HeroSection/>
+    <WelcomeSection/>
+    <WeeklySection/>
+    <SermonSection/>
+    <UpcomingSection/>
+    <LifeProcessSection/>
+    </>
   );
 }
