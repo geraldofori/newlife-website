81cecbd0670c030928ed4b1f41bb6bccffb4f431
geraldofori <oforige@gmail.com>
2025-09-12 17:14:52 +0000

    enhancement: updated backgroung image for weekly and hero section

--- components/ui/sections/WeeklySection.tsx
+++ components/ui/sections/WeeklySection.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const WeeklySection = () => {
   return (
-      <section className="w-full bg-[rgba(255,215,0,0.04)] py-20 px-4 lg:px-8">
+      <section className="w-full bg-[#FCFCFC] py-20 px-4 lg:px-8">
         <div className="max-w-7xl mx-auto">
           <h2 className="text-4xl lg:text-5xl font-bold text-black text-center mb-8">
             Our Weekly Gatherings
